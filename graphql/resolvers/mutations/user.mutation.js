a74bd56e018aba0ca9d29b5c2a8958872f117455
Mehmet Seven <mehmetseven0@gmail.com>
2018-10-22 13:51:08 +0300

    signIn mutation

--- graphql/resolvers/mutations/user.mutation.js
+++ graphql/resolvers/mutations/user.mutation.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+
 module.exports = {
 	createUser: async (parent, { data: { username, password } }, { User }) => {
 		const user = await User.findOne({ username });
@@ -10,5 +12,19 @@ module.exports = {
 			username,
 			password
 		}).save();
+	},
+
+	signIn: async (parent, { data: { username, password } }, { User }) => {
+		const user = await User.findOne({ username });
+		if (!user) {
+		  throw new Error("User does not exists!");
+		}
+
+		const validPassword = await bcrypt.compare(password, user.password);
+		if (!validPassword) {
+		  throw new Error("Wrong password!");
+		}
+
+		return { token: 'test token ljalsd' }
 	}
 };
