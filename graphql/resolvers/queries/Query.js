20058ca855e9066c09fd232b76717d572372aa45
Mehmet Seven <mehmetseven0@gmail.com>
2018-10-22 10:58:05 +0300

    snaps query

--- graphql/resolvers/queries/Query.js
+++ graphql/resolvers/queries/Query.js
@@ -8,7 +8,10 @@ const Query = {
 
 	snap: async (parent, args, { Snap }) => {
 		return await Snap.findById(args.id);
-	}
+	},
+	snaps: async (parent, args, { Snap }) => {
+		return await Snap.find({}).sort({ 'createdAt': 'desc'})
+	},
 };
 
 module.exports = Query;
