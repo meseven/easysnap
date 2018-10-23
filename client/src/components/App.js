c715a825008a7e3b0ca68b3cbc328fa92b0e526f
Mehmet Seven <mehmetseven0@gmail.com>
2018-10-23 15:09:56 +0300

    create react app

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        app
+      </div>
+    );
+  }
+}
+
+export default App;
