49920d17129eb116a6df3b293e0adcca9859e996
Mehmet Seven <mehmetseven0@gmail.com>
2018-10-27 00:23:28 +0300

    activeUser resolver

--- server/graphql/resolvers/queries/Query.js
+++ server/graphql/resolvers/queries/Query.js
@@ -5,6 +5,9 @@ const Query = {
 	users: async (parent, args, { User }) => {
 		return await User.find({}).sort({ 'createdAt': 'desc'})
 	},
+	activeUser: async (parent, args, { activeUser, User }) => {
+		return await User.findOne({ username: activeUser.username });
+	},
 
 	snap: async (parent, args, { Snap }) => {
 		return await Snap.findById(args.id);
