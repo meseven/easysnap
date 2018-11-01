9c5f6b1f6a7c00243f4c3ac9dc8def74d6dd8b56
Mehmet Seven <mehmetseven0@gmail.com>
2018-11-01 14:03:22 +0300

    mutation publish

--- server/graphql/resolvers/mutations/snap.mutation.js
+++ server/graphql/resolvers/mutations/snap.mutation.js
@@ -1,8 +1,18 @@
 module.exports = {
-	createSnap: async (parent, { data: { user_id, text } }, { Snap }) => {
-		return await new Snap({
-			user_id,
-			text
-		}).save();
+	createSnap: async (parent, { data: { user_id, text } }, { Snap, pubsub }) => {
+		try{
+			const snap = await new Snap({
+				user_id,
+				text
+			}).save();
+
+			pubsub.publish('sayi', {
+				sayi: 12
+			});
+
+			return snap;
+		}catch (e) {
+			throw new Error(e);
+		}
 	}
 };
--- server/graphql/resolvers/subscriptions/snap.subscription.js
+++ server/graphql/resolvers/subscriptions/snap.subscription.js
@@ -1,16 +1,6 @@
 module.exports = {
 	sayi: {
 		subscribe: (parent, args, { pubsub }) => {
-
-			let sayi = 5;
-
-			setInterval(() => {
-				sayi += 1;
-				pubsub.publish('sayi', {
-					sayi
-				});
-			}, 3000);
-
 			return pubsub.asyncIterator('sayi');
 		}
 	}
