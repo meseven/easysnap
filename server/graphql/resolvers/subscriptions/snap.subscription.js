4c701e3924886b3257d4e19ea1af11c0c5227c95
Mehmet Seven <mehmetseven0@gmail.com>
2018-11-02 12:42:19 +0300

    withFilter - apollo-server

--- server/graphql/resolvers/subscriptions/snap.subscription.js
+++ server/graphql/resolvers/subscriptions/snap.subscription.js
@@ -1,7 +1,14 @@
+const { withFilter } = require('apollo-server');
+
 module.exports = {
 	snapAdded: {
-		subscribe: (parent, args, { pubsub }) => {
-			return pubsub.asyncIterator('snap added');
-		}
+		subscribe: withFilter(
+			(parent, args, { pubsub }) => {
+				return pubsub.asyncIterator('snap added');
+			},
+			(payload, variables) => {
+				return variables.userId ? String(payload.snapAdded.user_id) === variables.userId : true ;
+			}
+		)
 	}
 };
